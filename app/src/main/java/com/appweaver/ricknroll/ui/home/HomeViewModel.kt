5e673cdbacefe21d7b0070813c0541ab92b50c7b
h4xor7 <saurabhpandey436@gmail.com>
2024-01-31 12:02:45 +0530

    implemented pallate ,glide and column

--- app/src/main/java/com/appweaver/ricknroll/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/appweaver/ricknroll/ui/home/HomeViewModel.kt
@@ -1,8 +1,13 @@
 package com.appweaver.ricknroll.ui.home
 
+import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
+import android.graphics.drawable.Drawable
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.graphics.Color
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import androidx.palette.graphics.Palette
 import com.appweaver.ricknroll.model.CharacterResponse
 import com.appweaver.ricknroll.repository.HomeRepository
 import com.appweaver.ricknroll.util.Resource
@@ -47,6 +52,16 @@ class HomeViewModel @Inject constructor(
                 }
             }
 
+        }
+    }
+
+    fun calcDominantColor(drawable: Drawable, onFinish: (Color) -> Unit) {
+        val bmp = (drawable as BitmapDrawable).bitmap.copy(Bitmap.Config.ARGB_8888, true)
+
+        Palette.from(bmp).generate { palette ->
+            palette?.dominantSwatch ?.rgb?.let { colorValue ->
+                onFinish(Color(colorValue))
+            }
         }
     }
 }
