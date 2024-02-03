8249dee1076c802024a5dd406eb2fa3fd084d865
h4xor7 <saurabhpandey436@gmail.com>
2024-02-03 08:59:18 +0530

    implemented text style

--- app/src/main/java/com/appweaver/ricknroll/ui/home/HomeScreen.kt
+++ app/src/main/java/com/appweaver/ricknroll/ui/home/HomeScreen.kt
@@ -29,6 +29,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -166,7 +167,10 @@ fun Character(
                 color =  colorScheme.onSurfaceVariant,
                 textAlign = TextAlign.Center,
                 fontWeight = FontWeight.Bold,
-                modifier = Modifier.fillMaxWidth()
+                modifier = Modifier.fillMaxWidth(),
+                maxLines = 1,
+                softWrap = true,
+                overflow = TextOverflow.Ellipsis
             )
 
         }
